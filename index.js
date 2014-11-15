224135017bd6fd859ba31b0576aa862f2c25f4a0
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-11-15 19:42:17 +0300

    Normalize deps

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var through2 = require('through2');
 var rod = require('require-or-die');
+var normalize = require('deps-normalize');
+var object = require('bem-object');
+
+function norm(self, deps) {
+    if (deps === undefined) { return []; }
+
+    if (!Array.isArray(deps)) { deps = [deps]; }
+
+    return normalize(deps.map(function (d) {
+        if (typeof d === 'string') {
+            if (d.indexOf('.') === -1) {
+                d += '.' + self.tech;
+            }
+            d = object(d);
+        }
+        return self.copy(d);
+    }));
+}
 
 module.exports = function (opts) {
     return through2.obj(function (obj, enc, cb) {
@@ -8,8 +26,8 @@ module.exports = function (opts) {
         rod(obj.path, opts, function (err, deps) {
             if (err) { return cb(err); }
 
-            obj.require = deps.mustDeps || deps.require;
-            obj.expect = deps.shouldDeps || deps.expect;
+            obj.require = norm(obj, deps.mustDeps || deps.require);
+            obj.expect = norm(obj, deps.shouldDeps || deps.expect);
 
             cb(null, obj);
         });
--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 var mock = require('mock-fs');
 var Deps = require('./index.js');
 var assert = require('assert');
+var object = require('bem-object');
 
 afterEach(function () {
     mock.restore();
@@ -11,22 +12,18 @@ afterEach(function () {
 it('should read file from fs', function (done) {
     mock({
         fixture: {
-            block: {
-                element: {
-                    'block__elem.deps.js': 'exports.shouldDeps = 1;'
-                }
-            }
+            'block__elem.deps.js': 'exports.shouldDeps = "1";'
         }
     });
 
     var deps = Deps();
-    deps.write({
-        path: 'fixture/block/element/block__elem.deps.js',
-        tech: 'deps.js'
-    });
+    deps.write(object('fixture/block__elem.deps.js'));
 
     deps.on('data', function (obj) {
-        assert.equal(obj.expect, 1);
+        assert.equal(obj.block, 'block');
+        assert.equal(obj.tech, 'deps.js');
+        assert.equal(obj.expect[0].block, '1');
+        assert.equal(obj.expect[0].tech, 'deps.js');
         done();
     });
 });
